88ded4c0f7d25b82ce5f8de0c8d0bc23b947b6bb
Jkker <j@erry.dev>
2023-03-12 00:43:54 -0500

    fix: db ttl

--- lib/db.ts
+++ lib/db.ts
@@ -2,18 +2,14 @@ import Keyv from 'keyv'
 import { KeyvFile } from 'keyv-file'
 import { isDev } from './config'
 
-let db: Keyv
-
-if (isDev) {
-  db = new Keyv({
-    store: new KeyvFile({
-      filename: 'cache.json', // file name
-      expiredCheckDelay: 1 * 3600 * 1000, // ms, check and remove expired data in each ms
-      writeDelay: 100, // ms, batch write to disk in a specific duration, enhance write performance.
-    }),
-  })
-} else {
-  db = new Keyv()
-}
+const db = new Keyv({
+  // use file store in dev
+  store: isDev
+    ? new KeyvFile({
+        filename: 'data/cache.json', // file name
+      })
+    : undefined,
+  ttl: 1000 * 60 * 60 * 1, // 1 hour,
+})
 
 export default db
