3bf24045c5a197302e62aeca52e495bcae6ca249
Jkker <j@erry.dev>
2023-03-28 20:20:42 -0400

    feat: replace posthog with vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,35 +4,13 @@ import '@fontsource/lexend/400.css'
 import '@fontsource/lexend/700.css'
 import { ThemeProvider } from 'next-themes'
 
-import { posthogConfig, posthogId } from '@/lib/config'
+import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import posthog from 'posthog-js'
-import { useEffect } from 'react'
 
 import { GlobalContextProvider } from '@/utils/useGlobal'
 import { ModalProvider } from '@/utils/useModal'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    function onRouteChangeComplete() {
-      if (posthogId) {
-        posthog.capture('$pageview')
-      }
-    }
-
-    if (posthogId) {
-      posthog.init(posthogId, posthogConfig)
-    }
-
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [router.events])
   return (
     <GlobalContextProvider>
       <ThemeProvider
@@ -44,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
       >
         <ModalProvider>
           <Component {...pageProps} />
+          <Analytics />
         </ModalProvider>
       </ThemeProvider>
     </GlobalContextProvider>
